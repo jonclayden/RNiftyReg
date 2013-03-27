65a2655ba76a27a97bb83b5af10b70f298536af8
Jon Clayden <jon.clayden@gmail.com>
2013-03-27 17:19:56 +0000

    More transform stuff (still WIP)

--- R/transform.R
+++ R/transform.R
@@ -1,20 +1,15 @@
-transformVoxelToWorld <- function (point, image, useOrigin = TRUE, ...)
+transformWithAffine <- function (points, affine, type)
+
+transformVoxelToWorld <- function (points, image, useOrigin = TRUE, ...)
 {
-    if (!is.nifti(image))
-        report(OL$Error, "The specified image is not a \"nifti\" object")
-    if (image@dim_[1] != 3)
-        report(OL$Error, "Only three-dimensional images can be used at present")
-    if (!is.numeric(point) || length(point) != 3)
-        report(OL$Error, "Point must be specified as a numeric vector of length 3")
-    
     affine <- xformToAffine(image, ...)
     if (!useOrigin)
         affine[1:3,4] <- 0
     
-    return (solve(affine) %*% point)
+    return (solve(affine) %*% points)
 }
 
-transformWorldToVoxel <- function (point, image, useOrigin = TRUE, ...)
+transformWorldToVoxel <- function (points, image, useOrigin = TRUE, ...)
 {
     if (!is.nifti(image))
         report(OL$Error, "The specified image is not a \"nifti\" object")
@@ -27,5 +22,5 @@ transformWorldToVoxel <- function (point, image, useOrigin = TRUE, ...)
     if (!useOrigin)
         affine[1:3,4] <- 0
     
-    return (affine %*% point)
+    return (affine %*% points)
 }
