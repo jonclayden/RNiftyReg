2b5766792594babffcc9d5d646d2a324a62a1ff9
Jon Clayden <code@clayden.org>
2015-07-21 23:21:25 +0100

    Updating tests for new images

--- tests/testthat/test-05-nifti.R
+++ tests/testthat/test-05-nifti.R
@@ -1,15 +1,15 @@
 context("Reading and writing NIfTI files")
 
 test_that("NIfTI files can be read and written", {
-    sourcePath <- system.file("extdata", "source.nii.gz", package="RNiftyReg")
+    imagePath <- system.file("extdata", "epi_t2.nii.gz", package="RNiftyReg")
     tempPath <- tempfile()
     
-    expect_that(dim(readNifti(sourcePath,internal=FALSE)), equals(c(96L,96L,25L)))
-    expect_that(dim(readNifti(sourcePath,internal=TRUE)), equals(c(96L,96L,25L)))
+    expect_that(dim(readNifti(imagePath,internal=FALSE)), equals(c(96L,96L,60L)))
+    expect_that(dim(readNifti(imagePath,internal=TRUE)), equals(c(96L,96L,60L)))
     
-    image <- readNifti(sourcePath)
+    image <- readNifti(imagePath)
     expect_that(pixunits(image), equals(c("mm","s")))
     writeNifti(image, tempPath)
-    expect_that(pixdim(readNifti(tempPath)), equals(c(2.5,2.5,5)))
+    expect_that(pixdim(readNifti(tempPath)), equals(c(2.5,2.5,2.5)))
     unlink(tempPath)
 })
--- tests/testthat/test-10-xform.R
+++ tests/testthat/test-10-xform.R
@@ -1,14 +1,14 @@
 context("NIfTI sform/qform operations")
 
 test_that("NIfTI sform/qform operations work", {
-    image <- readNifti(system.file("extdata", "source.nii.gz", package="RNiftyReg"))
+    image <- readNifti(system.file("extdata", "epi_t2.nii.gz", package="RNiftyReg"))
     
-    expect_that(diag(xform(image)), equals(c(-2.5,2.5,5,1)))
-    expect_that(diag(xform(image,useQuaternionFirst=FALSE)), equals(c(-2.5,2.5,5,1)))
+    expect_that(diag(xform(image)), equals(c(-2.5,2.5,2.5,1)))
+    expect_that(diag(xform(image,useQuaternionFirst=FALSE)), equals(c(-2.5,2.5,2.5,1)))
     
-    point <- c(40, 40, 10)
-    expect_that(voxelToWorld(point,image), equals(c(-97.5,97.5,45.0)))
-    expect_that(voxelToWorld(point,image,simple=TRUE), equals(c(97.5,97.5,45.0)))
+    point <- c(40, 40, 20)
+    expect_that(round(voxelToWorld(point,image)), equals(c(25,2,-8)))
+    expect_that(voxelToWorld(point,image,simple=TRUE), equals(c(97.5,97.5,47.5)))
     expect_that(worldToVoxel(voxelToWorld(point,image),image), equals(point))
     expect_that(worldToVoxel(voxelToWorld(point,image,simple=TRUE),image,simple=TRUE), equals(point))
 })
