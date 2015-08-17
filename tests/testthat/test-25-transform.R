6b0f3dd5027fa13e2c79a43f76c4a15b1339beb1
Jon Clayden <code@clayden.org>
2015-08-17 15:33:31 +0100

    Adding a further test

--- tests/testthat/test-25-transform.R
+++ tests/testthat/test-25-transform.R
@@ -17,6 +17,9 @@ test_that("Existing transformations can be applied and combined", {
     expect_that(applyTransform(t1_to_mni,point,nearest=TRUE), equals(c(33,49,24)))
     expect_that(round(applyTransform(t1_to_mni,point,nearest=FALSE)), equals(c(33,49,24)))
     
+    # Different z-value due to double-rounding
+    expect_that(applyTransform(t1_to_mni,t1,interpolation=0)[33,49,25], equals(t1[34,49,64]))
+    
     t2_to_mni <- composeTransforms(t2_to_t1, t1_to_mni)
     expect_that(applyTransform(t2_to_mni,c(40,40,20),nearest=TRUE), equals(c(33,49,24)))
     
