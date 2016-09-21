ea370bfbc27bb1187f617390917cedb8218270d8
Jon Clayden <code@clayden.org>
2016-09-21 11:22:08 +0100

    Exporting RNifti functions for consistency with previous versions

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,8 @@
 #' @importFrom Rcpp evalCpp
 #' @importFrom splines bs
 #' @importFrom stats lm na.omit predict
-#' @importFrom utils object.size read.table
+#' @importFrom utils read.table
+#' @export "pixdim<-" "pixunits<-" "qform<-" "sform<-" dumpNifti ndim pixdim pixunits retrieveNifti updateNifti voxelToWorld worldToVoxel writeNifti xform
 #' @useDynLib RNiftyReg
 .onLoad <- function (libname, pkgname)
 {
