4c7a7afd3049c86b899aa3443ecc12cc8ed735b3
Jon Clayden <code@clayden.org>
2016-09-21 17:40:14 +0100

    Documenting re-exported functions

--- R/zzz.R
+++ R/zzz.R
@@ -3,9 +3,37 @@
 #' @importFrom splines bs
 #' @importFrom stats lm na.omit predict
 #' @importFrom utils read.table
-#' @export "pixdim<-" "pixunits<-" "qform<-" "sform<-" dumpNifti ndim pixdim pixunits retrieveNifti updateNifti voxelToWorld worldToVoxel writeNifti xform
 #' @useDynLib RNiftyReg
 .onLoad <- function (libname, pkgname)
 {
     .Call("initialise", PACKAGE="RNiftyReg")
 }
+
+#' @export
+RNifti::`pixdim<-`
+#' @export
+RNifti::`pixunits<-`
+#' @export
+RNifti::`qform<-`
+#' @export
+RNifti::`sform<-`
+#' @export
+RNifti::dumpNifti
+#' @export
+RNifti::ndim
+#' @export
+RNifti::pixdim
+#' @export
+RNifti::pixunits
+#' @export
+RNifti::retrieveNifti
+#' @export
+RNifti::updateNifti
+#' @export
+RNifti::voxelToWorld
+#' @export
+RNifti::worldToVoxel
+#' @export
+RNifti::writeNifti
+#' @export
+RNifti::xform
